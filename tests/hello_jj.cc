95f86e0ab44fdd60a72d497d45b9c2e7995ba0e8
Hao Jiang <joshtrick@outlook.com>
2019-12-20 17:06:03 +0800

    Update hello_jj.cc

--- tests/hello_jj.cc
+++ tests/hello_jj.cc
@@ -10,20 +10,28 @@ using namespace std;
 int main()
 {
   joshtrick::Timer global_timer("Testing loop");
+  float instant_t = 0;
+  float average_t = 0;
+  float total_t   = 0;
+
   print_joshtrick();
   cout << MSG_TAG
     << "Hello Joshtrick"
     << endl;
   while(true)
   {
-  global_timer.tic();
-  sleep(1);
-  global_timer.toc();
-  global_timer.calculate();
-  cout << global_timer.instant() << endl;
-  cout << global_timer.average() << endl;
-  cout << global_timer.total() << endl;
-  cout << endl;
+    global_timer.tic();
+    sleep(5);
+    global_timer.toc();
+    global_timer.calculate();
+    instant_t = global_timer.instant();
+    average_t = global_timer.average();
+    total_t = global_timer.total();
+    cout << endl;
+    cout << "Instant: " << instant_t << " us" << endl;
+    cout << "Average: " << average_t << " us" << endl;
+    cout << "Total  : " << total_t << " us" << endl;
+    cout << "~~~~~~~~" << endl;
   }
 
   return 0;
